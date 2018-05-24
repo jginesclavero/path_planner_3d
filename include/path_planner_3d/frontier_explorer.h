f9beb61f11f7a2fbaeb7914c3c78d029f1b3dc3a
jgines <jonathangines@hotmail.com>
2018-05-24 22:52:38 +0200

    fix frontier explorer

--- include/path_planner_3d/frontier_explorer.h
+++ include/path_planner_3d/frontier_explorer.h
@@ -7,6 +7,8 @@
 #include <tf/message_filter.h>
 #include <message_filters/subscriber.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <map_msgs/OccupancyGridUpdate.h>
+
 #include <algorithm>
 #include <time.h>
 
@@ -25,15 +27,17 @@ public:
 
 	FrontierExplorer();
 
-	virtual void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& short_map);
+	void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& short_map);
+	void globalMapCallback(const nav_msgs::OccupancyGrid::ConstPtr& map);
+	void globalMapUpdatesCallback(const map_msgs::OccupancyGridUpdate::ConstPtr& map);
 	virtual void step();
 
 private:
 	ros::NodeHandle nh_;
 	ros::NodeHandle private_nh_;
-	ros::Subscriber map_sub;
+	ros::Subscriber map_sub,global_map_sub,global_map_updates_sub;
 	ros::Publisher 	frontierMap_pub,vis_pub_,path_pub;
-	costmap_2d::Costmap2D cost_map;
+	costmap_2d::Costmap2D cost_map_,global_costmap;
 	costmap_2d::Costmap2DPublisher cost_map_publisher_;
 	std::list<geometry_msgs::Point> frontierPList;
 	std::map<int,std::list<geometry_msgs::Point>> frontierMap;
@@ -41,9 +45,9 @@ private:
 	float resolution;
 	geometry_msgs::Point originMap;
 	nav_msgs::Path path;
-	bool debug_mode;
+	bool debug_mode, global_costmap_ready,map_ready;
 
-	void grid2CostMap(nav_msgs::OccupancyGrid map);
+	void grid2CostMap(nav_msgs::OccupancyGrid map, costmap_2d::Costmap2D& cost_map);
 	void frontierDetector();
 	void frontierClass();
 	bool isFrontier(int x, int y);
@@ -52,7 +56,7 @@ private:
 	void getRefFrontierPoints();
 	void frontierVis();
 	void addLocationVis(int id, geometry_msgs::Point point,float r, float g, float b,float alpha);
-
+	bool isFrontierPointFree(geometry_msgs::Point p);
 
 	//nav_msgs::OccupancyGrid static_map,longTerm_map,shortTerm_map,effective_map;
 
--- src/frontier_explorer.cpp
+++ src/frontier_explorer.cpp
@@ -1,12 +1,17 @@
 #include "path_planner_3d/frontier_explorer.h"
 
 FrontierExplorer::FrontierExplorer():
-private_nh_("~"),cost_map_publisher_(&nh_,&cost_map,"/map","/costmap_auto",true){
-	map_sub	= nh_.subscribe("/map", 5, &FrontierExplorer::mapCallback,this);
-	vis_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("markers", 10,true);
+private_nh_("~"),cost_map_publisher_(&nh_,&cost_map_,"/map","/costmap_auto",true){
+	map_sub	= nh_.subscribe("/map", 1, &FrontierExplorer::mapCallback,this);
+	global_map_updates_sub	= nh_.subscribe("/move_base/global_costmap/costmap_updates", 1, &FrontierExplorer::globalMapUpdatesCallback,this);
+	global_map_sub	= nh_.subscribe("/move_base/global_costmap/costmap", 1, &FrontierExplorer::globalMapCallback,this);
+
+	vis_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("markers", 1,false);
 	path_pub = nh_.advertise<nav_msgs::Path>("frontier_path",1,true);
 	//if (nh_.getParam("debug_mode", debug_mode));
-	debug_mode = true;
+	debug_mode = false;
+	global_costmap_ready = false;
+	map_ready = false;
 	nodes_vis_.markers.clear();
 }
 
@@ -15,14 +20,36 @@ FrontierExplorer::mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& map){
 	resolution = map->info.resolution;
 	originMap.x =  map->info.origin.position.x;
 	originMap.y =  map->info.origin.position.y;
-	grid2CostMap(*map);
+	grid2CostMap(*map,cost_map_);
+	map_ready = true;
 }
 
 void
-FrontierExplorer::grid2CostMap(nav_msgs::OccupancyGrid map){
+FrontierExplorer::globalMapCallback(const nav_msgs::OccupancyGrid::ConstPtr& map){
+	//resolution = map->info.resolution;
+	//originMap.x =  map->info.origin.position.x;
+	//originMap.y =  map->info.origin.position.y;
+	ROS_ERROR("global_costmap received");
+	grid2CostMap(*map,global_costmap);
+	global_costmap_ready = true;
+}
+
+void
+FrontierExplorer::globalMapUpdatesCallback(const map_msgs::OccupancyGridUpdate::ConstPtr& map){
+	float normalizer = 2.54;
+	int i = 0;
+	for(int y=map->y; y< map->y+map->height; y++){
+  	for(int x=map->x; x< map->x+map->width; x++){
+			global_costmap.setCost(x,y,round(normalizer*map->data[i++]));
+		}
+	}
+}
+
+void
+FrontierExplorer::grid2CostMap(nav_msgs::OccupancyGrid map, costmap_2d::Costmap2D& cost_map){
 	cost_map.resizeMap(map.info.width,map.info.height, map.info.resolution, map.info.origin.position.x, map.info.origin.position.y);
-	ROS_INFO("origin x: %f y:%f",map.info.origin.position.x, map.info.origin.position.y);
 	cost_map.setDefaultValue(254);
+	//ROS_ERROR("size x:%u y:%u",cost_map.getSizeInCellsX(),cost_map.getSizeInCellsY());
 	int i = 0;
 	float normalizer = 2.54;
 	for(int y=0;y<cost_map.getSizeInCellsY();y++){
@@ -38,10 +65,10 @@ FrontierExplorer::grid2CostMap(nav_msgs::OccupancyGrid map){
 
 void
 FrontierExplorer::frontierDetector(){
-	for(int y=0;y<cost_map.getSizeInCellsY();y++){
-		for(int x=0;x<cost_map.getSizeInCellsX();x++){
-			if(cost_map.getCost(x,y) == 0 && isFrontier(x,y)){
-				cost_map.setCost(x,y,50);
+	for(int y=0;y<cost_map_.getSizeInCellsY();y++){
+		for(int x=0;x<cost_map_.getSizeInCellsX();x++){
+			if(cost_map_.getCost(x,y) == 0 && isFrontier(x,y)){
+				cost_map_.setCost(x,y,50);
 				geometry_msgs::Point p;
 				p.x = x;
 				p.y = y;
@@ -80,16 +107,16 @@ FrontierExplorer::frontierClass(){
 
 bool
 FrontierExplorer::isFrontier(int x, int y){
-	return (cost_map.getCost(x+1,y) == 255 || cost_map.getCost(x-1,y) == 255 ||
-	 cost_map.getCost(x,y+1) == 255 || cost_map.getCost(x,y-1) == 255);
+	return (cost_map_.getCost(x+1,y) == 255 || cost_map_.getCost(x-1,y) == 255 ||
+	 cost_map_.getCost(x,y+1) == 255 || cost_map_.getCost(x,y-1) == 255);
 }
 
 bool
 FrontierExplorer::isEndFrontier(geometry_msgs::Point p){
-	return (cost_map.getCost(p.x+1,p.y) == 254 || cost_map.getCost(p.x+1,p.y+1) == 254 ||
-		cost_map.getCost(p.x,p.y+1) == 254 || cost_map.getCost(p.x-1,p.y+1) == 254 ||
-		cost_map.getCost(p.x-1,p.y) == 254 || cost_map.getCost(p.x-1,p.y-1) == 254 ||
-		cost_map.getCost(p.x,p.y-1) == 254 || cost_map.getCost(p.x+1,p.y-1) == 254);
+	return (cost_map_.getCost(p.x+1,p.y) == 254 || cost_map_.getCost(p.x+1,p.y+1) == 254 ||
+		cost_map_.getCost(p.x,p.y+1) == 254 || cost_map_.getCost(p.x-1,p.y+1) == 254 ||
+		cost_map_.getCost(p.x-1,p.y) == 254 || cost_map_.getCost(p.x-1,p.y-1) == 254 ||
+		cost_map_.getCost(p.x,p.y-1) == 254 || cost_map_.getCost(p.x+1,p.y-1) == 254);
 }
 
 bool
@@ -98,6 +125,19 @@ FrontierExplorer::isNeighbor(geometry_msgs::Point p1,geometry_msgs::Point p2){
 	p.x = p2.x - p1.x;
 	p.y = p2.y - p1.y;
 	return (sqrt(powf(p.x, 2) + powf(p.y, 2)) < 1.5);
+}
+
+bool
+FrontierExplorer::isFrontierPointFree(geometry_msgs::Point p){
+	unsigned int x_costmap,y_costmap;
+	global_costmap.worldToMap(p.x,p.y,x_costmap,y_costmap);
+	unsigned int cost = global_costmap.getCost(x_costmap,y_costmap);
+	if (cost==0){
+		return true;
+	}else{
+		return false;
+
+	}
 }
 
 void
@@ -119,7 +159,6 @@ FrontierExplorer::addLocationVis(int id, geometry_msgs::Point point,float r, flo
 	visualization_msgs::Marker marker;
 	geometry_msgs::	Pose p;
 	marker.header.frame_id = "/map";
-	marker.header.stamp = ros::Time();
 	marker.ns = "locations";
 	marker.id = id;
 	marker.type = visualization_msgs::Marker::SPHERE;
@@ -140,8 +179,9 @@ void
 FrontierExplorer::getRefFrontierPoints(){
 	geometry_msgs::PoseStamped pPath;
 	geometry_msgs::Point p;
+	std::list<geometry_msgs::PoseStamped> pList;
 	int num_points = 0, i=0;
-	path.poses.resize(frontierMap.size());
+
 	for (std::map<int,std::list<geometry_msgs::Point>>::iterator itMap=frontierMap.begin(); itMap!=frontierMap.end(); ++itMap){
 		for (std::list<geometry_msgs::Point>::iterator it=itMap->second.begin(); it != itMap->second.end(); ++it){
 			p.x = p.x + (it->x * resolution + originMap.x);
@@ -152,13 +192,22 @@ FrontierExplorer::getRefFrontierPoints(){
 		p.x = p.x / num_points;
 		p.y = p.y / num_points;
 		pPath.pose.position = p;
-		path.poses[i] = pPath;
-		addLocationVis(i,p,0.0,1.0,0.0,1.0);
+
+		if(isFrontierPointFree(pPath.pose.position)){
+			pList.push_back(pPath);
+			addLocationVis(i,pPath.pose.position,0.0,1.0,0.0,1.0);
+		}
 		num_points = 0;
 		p.x = 0;
 		p.y = 0;
 		i++;
 	}
+
+	path.poses.resize(pList.size());
+	i=0;
+	for (std::list<geometry_msgs::PoseStamped>::iterator it=pList.begin(); it != pList.end(); ++it)
+		path.poses[i++] = *it;
+
 }
 
 void
@@ -172,6 +221,9 @@ FrontierExplorer::publishAll(){
 
 void
 FrontierExplorer::step(){
+	if(!global_costmap_ready || !map_ready){
+		return;
+	}
 	frontierDetector();
 	frontierClass();
 	getRefFrontierPoints();
@@ -182,6 +234,7 @@ FrontierExplorer::step(){
 
 	publishAll();
 	frontierPList.clear();
+	path.poses.clear();
 }
 
 
